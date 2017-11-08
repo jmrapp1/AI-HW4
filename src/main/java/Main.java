144867219b730dbd95606080bfaa6c0f195bba13
jmrapp1 <jmrapp1270@gmail.com>
2017-11-08 15:43:23 -0500

    switched to gradle project

---
+++ src/main/java/Main.java
@@ -0,0 +1,35 @@
+import org.jenetics.BitChromosome;
+import org.jenetics.BitGene;
+import org.jenetics.Genotype;
+import org.jenetics.engine.Engine;
+import org.jenetics.engine.EvolutionResult;
+import org.jenetics.util.Factory;
+
+public class Main {
+
+    private static int eval(Genotype<BitGene> gt) {
+        return gt.getChromosome()
+                .as(BitChromosome.class)
+                .bitCount();
+    }
+
+    public static void main(String[] args) {
+        // 1.) Define the genotype (factory) suitable
+        //     for the problem.
+        Factory<Genotype<BitGene>> gtf =
+                Genotype.of(BitChromosome.of(10, 0.5));
+
+        // 3.) Create the execution environment.
+        Engine<BitGene, Integer> engine = Engine
+                .builder(Main::eval, gtf)
+                .build();
+
+        // 4.) Start the execution (evolution) and
+        //     collect the result.
+        Genotype<BitGene> result = engine.stream()
+                .limit(100)
+                .collect(EvolutionResult.toBestGenotype());
+
+        System.out.println("Hello World:\n" + result);
+    }
+}
