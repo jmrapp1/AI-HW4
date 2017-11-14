afd82c756a2c013235898a27629184c9b7bfac1a
jmrapp1 <jmrapp1270@gmail.com>
2017-11-14 14:39:29 -0500

    big int

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,17 +24,25 @@ public class Main {
                 .builder(Main::eval, gtf)
                 .populationSize(50)
                 .alterers(
-                    new Crossover<>(0.1),
+                    // new Crossover<>(0.1),
                     new Mutator<>(0.001)
                 )
                 .build();
 
-        System.out.println(gtf);
+        // System.out.println(gtf);
         // 4.) Start the execution (evolution) and
         //     collect the result.
         Genotype<BitGene> result = engine.stream()
                 .limit(100)
                 .collect(EvolutionResult.toBestGenotype());
         System.out.println(result);
+        System.out.println(result.getChromosome().as(BitChromosome.class).toBigInteger());
     }
+
+    /*public int getXValue(BitChromosome chromosome, int startIndex, int endIndex) {
+        for (int i = startIndex; i < endIndex; i++) {
+
+        }
+    }*/
+
 }
